f311bc5a646634123aefb9f8644cf05170a24ce1
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-06-12 18:19:54 +0300

    Create custom BottomView

---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/AuthScene/Controllers/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Evgeniy Fakhretdinov on 11.06.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    private let bottomView = BottomView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+
+    }
+
+    func facebookPress() {
+        print("facebook")
+    }
+    func googlePress() {
+        print("google")
+    }
+
+
+}
+
+private extension LoginViewController {
+    func setupLayout() {
+        setupBottomView()
+    }
+    func setupBottomView() {
+        view.addSubview(bottomView)
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+
+        bottomView.button2Action = facebookPress
+        bottomView.button1Action = googlePress
+
+        NSLayoutConstraint.activate([
+            bottomView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            bottomView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            bottomView.heightAnchor.constraint(equalToConstant: 170)
+        ])
+    }
+}
+
+
+#Preview("LoginVC") {
+    LoginViewController()
+}
